2c06ac947b00178ed4df3a94f413dcf98379b00c
Widiang27 <widiangajaa@gmail.com>
2026-05-26 18:21:31 +0700

    first commit

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-let express = require('express');
-let mysql = require('mysql2');
+const express = require('express');
+const mysql = require('mysql2');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -9,38 +9,141 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(express.static('public'));
 
-// MySQL connection
-let con = mysql.createConnection({
+const con = mysql.createConnection({
     host: "localhost",
     user: "root",
     password: "",
     database: "db_cuci_sepatu"
 });
 
-con.connect(function (err) {
+con.connect((err) => {
     if (err) throw err;
     console.log("Database Connected!");
 });
+// =======================
+// LOGIN
+// =======================
+app.post('/login', (req, res) => {
 
-// tambah pelanggan
-app.post('/addpelanggan', (req, res) => {
-    const { nama_pelanggan, email } = req.body;
+    const { email, password } = req.body;
 
-    const sql = 'INSERT INTO pelanggan (nama_pelanggan, email) VALUES (?, ?)';
+    const sql = `
+        SELECT * FROM user
+        WHERE email = ? AND password = ?
+    `;
 
-    con.query(sql, [nama_pelanggan, email], (err, result) => {
-        if (err) {
-            res.send(err);
-            return;
+    con.query(
+        sql,
+        [email, password],
+        (err, results) => {
+
+            if (err) {
+                res.status(500).json({
+                    success: false,
+                    message: err
+                });
+
+                return;
+            }
+
+            // cek user ada atau tidak
+            if (results.length > 0) {
+
+                res.json({
+                    success: true,
+                    message: 'Login berhasil',
+                    user: results[0]
+                });
+
+            } else {
+
+                res.json({
+                    success: false,
+                    message: 'Email atau password salah'
+                });
+
+            }
         }
-
-        res.send('pelanggan Added');
-    });
+    );
 });
 
-// ambil data pelanggan
+
+// =======================
+// CREATE PELANGGAN
+// =======================
+app.post('/addpelanggan', (req, res) => {
+
+    const {
+        username,
+        email,
+        password,
+        no_telp,
+        alamat,
+        nama_lengkap_pelanggan
+    } = req.body;
+
+    const sqlUser = `
+        INSERT INTO user
+        (username, email, password, no_telp, alamat)
+        VALUES (?, ?, ?, ?, ?)
+    `;
+
+    con.query(
+        sqlUser,
+        [username, email, password, no_telp, alamat],
+        (err, result) => {
+
+            if (err) {
+                res.send(err);
+                return;
+            }
+
+            const id_user = result.insertId;
+
+            const sqlPelanggan = `
+                INSERT INTO pelanggan
+                (id_user, nama_lengkap_pelanggan)
+                VALUES (?, ?)
+            `;
+
+            con.query(
+                sqlPelanggan,
+                [id_user, nama_lengkap_pelanggan],
+                (err2) => {
+
+                    if (err2) {
+                        res.send(err2);
+                        return;
+                    }
+
+                    res.send("Pelanggan berhasil ditambahkan");
+                }
+            );
+        }
+    );
+});
+
+
+// =======================
+// READ PELANGGAN
+// =======================
 app.get('/pelanggan', (req, res) => {
-    con.query('SELECT * FROM pelanggan', (err, results) => {
+
+    const sql = `
+        SELECT 
+            pelanggan.id_user,
+            pelanggan.nama_lengkap_pelanggan,
+            user.username,
+            user.email,
+            user.password,
+            user.no_telp,
+            user.alamat
+        FROM pelanggan
+        JOIN user ON pelanggan.id_user = user.id_user
+    `;
+
+    con.query(sql, (err, results) => {
+
         if (err) {
             res.send(err);
             return;
@@ -50,7 +153,98 @@ app.get('/pelanggan', (req, res) => {
     });
 });
 
-// Start server
+
+// =======================
+// UPDATE PELANGGAN
+// =======================
+app.put('/updatepelanggan/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    const {
+        nama_lengkap_pelanggan,
+        username,
+        email,
+        password,
+        no_telp,
+        alamat
+    } = req.body;
+
+    const sqlUser = `
+        UPDATE user
+        SET
+            username = ?,
+            email = ?,
+            password = ?,
+            no_telp = ?,
+            alamat = ?
+        WHERE id_user = ?
+    `;
+
+    con.query(
+        sqlUser,
+        [username, email, password, no_telp, alamat, id],
+        (err) => {
+
+            if (err) {
+                res.send(err);
+                return;
+            }
+
+            const sqlPelanggan = `
+                UPDATE pelanggan
+                SET nama_lengkap_pelanggan = ?
+                WHERE id_user = ?
+            `;
+
+            con.query(
+                sqlPelanggan,
+                [nama_lengkap_pelanggan, id],
+                (err2) => {
+
+                    if (err2) {
+                        res.send(err2);
+                        return;
+                    }
+
+                    res.send("Data berhasil diupdate");
+                }
+            );
+        }
+    );
+});
+
+
+// =======================
+// DELETE PELANGGAN
+// =======================
+app.delete('/deletepelanggan/:id', (req, res) => {
+
+    const id = req.params.id;
+
+    // hapus dari tabel user
+    // pelanggan otomatis ikut terhapus karena foreign key cascade
+
+    const sql = `
+        DELETE FROM user
+        WHERE id_user = ?
+    `;
+
+    con.query(sql, [id], (err) => {
+
+        if (err) {
+            res.send(err);
+            return;
+        }
+
+        res.send("Data berhasil dihapus");
+    });
+});
+
+
+// =======================
+// START SERVER
+// =======================
 app.listen(3000, () => {
     console.log('Server running on port 3000');
 });
